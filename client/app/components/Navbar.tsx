74362ce2dc33801c23055ba5e162a3dfe891bf1a
Gursimran Singh <sgursimranmatharu@gmail.com>
2025-06-25 19:10:19 +0530

    fix: Add relative positioning to Navbar for improved layout; remove unused import in RepoListClient

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import ClientNavbarButtons from "./ClientNavbarButtons";
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between mx-auto max-w-[1400px] m-4">
+        <nav className="flex items-center justify-between mx-auto max-w-[1400px] m-4 relative z-50">
             <div className="flex p-2 gap-2 cursor-pointer select-none">
                 <Image src={Logo} alt="Logo" width={35} height={35} />
                 <h1 className="text-2xl uppercase font-jura text-white">Agamify</h1>
